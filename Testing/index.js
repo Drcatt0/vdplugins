4476854a429c1152e10a6a93954d86101ca4273e
Drcatto <coiersill0@gmail.com>
2024-11-22 19:48:17 -0800

    Update index.js

--- Testing/index.js
+++ Testing/index.js
@@ -8,7 +8,176 @@
     const FluxDispatcher = d.findByProps("dispatch", "subscribe");
 
     // Storage for settings
-    const Settings = r.createProxy({
+    const Settings = r.createProxy({(function (p, r, s, e, u, E, D, S, P) {
+    "use strict";
+
+    const { getGuildFolders } = s.findByStoreName("UserSettingsProtoStore");
+    const { isFolderExpanded } = s.findByStoreName("ExpandedGuildFolderStore");
+    const FluxDispatcher = e.FluxDispatcher;
+    const { View, Image } = D.General;
+    const { FormSwitch, FormRow, FormIcon, FormTextInput } = D.Forms;
+    const { ScrollView } = e.ReactNative;
+    const { useState } = e.React;
+
+    r.storage.autoCollapse ??= false;
+    r.storage.hideIcons ??= false;
+    r.storage.folderIcons ??= {};
+
+    function applyCustomIcons() {
+        getGuildFolders().forEach((folder) => {
+            if (r.storage.folderIcons[folder.folderId]) {
+                FluxDispatcher.dispatch({
+                    type: "CUSTOM_FOLDER_ICON_UPDATE",
+                    folderId: folder.folderId,
+                    icon: r.storage.folderIcons[folder.folderId],
+                });
+            }
+        });
+    }
+
+    function y() {
+        getGuildFolders()
+            .filter((folder) => folder.folderId && !isFolderExpanded(folder.folderId))
+            .forEach((folder) => {
+                FluxDispatcher.dispatch({
+                    type: "TOGGLE_GUILD_FOLDER_EXPAND",
+                    folderId: folder.folderId,
+                });
+                FluxDispatcher.dispatch({
+                    type: "TOGGLE_GUILD_FOLDER_EXPAND",
+                    folderId: folder.folderId,
+                });
+            });
+    }
+
+    const FolderSettingsRow = ({ folder }) => {
+        const [icon, setIcon] = useState(r.storage.folderIcons[folder.folderId] || "");
+
+        const handleIconChange = (value) => {
+            setIcon(value);
+            r.storage.folderIcons[folder.folderId] = value;
+            applyCustomIcons();
+        };
+
+        return (
+            <FormRow
+                label={`Folder: ${folder.name || `Unnamed (${folder.folderId})`}`}
+                trailing={
+                    <FormTextInput
+                        placeholder="Paste icon URL"
+                        value={icon}
+                        onChange={handleIconChange}
+                    />
+                }
+            />
+        );
+    };
+
+    function A() {
+        let unpatch = E.after(
+            "GuildContainer",
+            s.findByPropsAll("GuildContainer").at(-1),
+            function ([{ children }], res) {
+                const folder = children?.props?.folder;
+                if (folder && r.storage.folderIcons[folder.folderId]) {
+                    const customIcon = r.storage.folderIcons[folder.folderId];
+                    const iconStyle = {
+                        overflow: "hidden",
+                        borderRadius: 16,
+                        justifyContent: "center",
+                        alignItems: "center",
+                        height: 48,
+                        width: 48,
+                    };
+
+                    const icon = React.createElement(Image, {
+                        source: { uri: customIcon },
+                        style: { tintColor: folder.folderColor ? `#${folder.folderColor.toString(16)}` : undefined },
+                    });
+
+                    res.children = React.createElement(View, { style: iconStyle }, icon);
+                }
+            }
+        );
+        return y(), function () {
+            setTimeout(function () {
+                return y();
+            }, 100);
+            unpatch();
+        };
+    }
+
+    function X() {
+        S.useProxy(r.storage);
+        const [autoCollapse, setAutoCollapse] = useState(r.storage.autoCollapse);
+        const [hideIcons, setHideIcons] = useState(r.storage.hideIcons);
+        const folders = getGuildFolders().filter((folder) => folder.folderId);
+
+        function toggleAutoCollapse() {
+            r.storage.autoCollapse = !r.storage.autoCollapse;
+            setAutoCollapse(r.storage.autoCollapse);
+        }
+
+        function toggleHideIcons() {
+            r.storage.hideIcons = !r.storage.hideIcons;
+            setHideIcons(r.storage.hideIcons);
+        }
+
+        return (
+            <ScrollView style={{ flex: 1, marginTop: 10 }}>
+                <FormRow
+                    label="Auto Collapse Folders"
+                    subLabel="Automatically collapse other folders when expanding one."
+                    trailing={
+                        <FormSwitch
+                            value={autoCollapse}
+                            onValueChange={toggleAutoCollapse}
+                        />
+                    }
+                />
+                <FormRow
+                    label="Hide Folder Icons"
+                    subLabel="Hide server icons for collapsed folders."
+                    trailing={
+                        <FormSwitch
+                            value={hideIcons}
+                            onValueChange={toggleHideIcons}
+                        />
+                    }
+                />
+                {folders.map((folder) => (
+                    <FolderSettingsRow key={folder.folderId} folder={folder} />
+                ))}
+            </ScrollView>
+        );
+    }
+
+    let unpatches = [];
+
+    var j = {
+        onLoad: function () {
+            unpatches.push(A());
+        },
+        onUnload: function () {
+            for (const unpatch of unpatches) unpatch();
+            unpatches = [];
+        },
+        settings: X,
+    };
+
+    return (p.default = j), Object.defineProperty(p, "__esModule", { value: !0 }), p;
+})(
+    {},
+    vendetta.plugin,
+    vendetta.metro,
+    vendetta.metro.common,
+    vendetta.ui.assets,
+    vendetta.patcher,
+    vendetta.ui.components,
+    vendetta.storage,
+    vendetta.ui
+);
+
         autoCollapse: false,
         folders: {},
     });
