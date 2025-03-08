6a03a10f2715e8bb9c1bf686ec631333641f8750
Drcatto <coiersill0@gmail.com>
2025-03-08 14:44:03 -0700

    Update index.js

--- Testing/index.js
+++ Testing/index.js
@@ -1,73 +1,48 @@
 (function (plugin, v, m) {
     "use strict";
 
-    console.log("[Testing Plugin] Plugin loaded and running!");
-
     const { React } = v.metro.common;
     const ProfileModule = m.findByProps("openProfileSheet");
     const UserStore = m.findByProps("getCurrentUser");
-    const ChatInputActions = m.findByName("ChatInputActions");
+    const ChatInputActions = m.findByName("ChatInputActions"); // The component we are injecting into
 
     let unpatch;
 
-    function injectProfileButton() {
-        console.log("[Testing Plugin] Attempting to inject profile button...");
-
+    function injectDebugText() {
         if (!ChatInputActions) {
-            console.error("[Testing Plugin] ChatInputActions component not found!");
             return;
         }
 
         unpatch = v.patcher.after("default", ChatInputActions, (_, res) => {
-            console.log("[Testing Plugin] ChatInputActions found, modifying...");
-
             if (!res || !res.props || !res.props.children) {
-                console.error("[Testing Plugin] ChatInputActions has no children!");
                 return res;
             }
 
-            const currentUser = UserStore.getCurrentUser();
-            if (!currentUser) {
-                console.error("[Testing Plugin] Unable to get current user!");
-                return res;
-            }
-
-            console.log("[Testing Plugin] Injecting avatar button...");
-
-            const avatarUrl = `https://cdn.discordapp.com/avatars/${currentUser.id}/${currentUser.avatar}.png?size=32`;
-
-            // Create the avatar button
-            const AvatarButton = React.createElement(
-                "img",
+            // Create a debug message
+            const DebugText = React.createElement(
+                "div",
                 {
-                    src: avatarUrl,
                     style: {
-                        width: 32,
-                        height: 32,
-                        borderRadius: "50%",
-                        marginRight: 8,
-                        cursor: "pointer",
-                    },
-                    onClick: () => ProfileModule.openProfileSheet(currentUser.id),
-                }
+                        color: "red",
+                        fontSize: 16,
+                        fontWeight: "bold",
+                        padding: 4,
+                    }
+                },
+                "Hello from Debug!"
             );
 
-            // Inject the button at the **start** of the action buttons
-            res.props.children.unshift(AvatarButton);
-            console.log("[Testing Plugin] Profile button added!");
+            // Inject the debug message into the chat bar
+            res.props.children.unshift(DebugText);
             return res;
         });
-
-        console.log("[Testing Plugin] Injection complete.");
     }
 
     plugin.onLoad = function () {
-        console.log("[Testing Plugin] Plugin onLoad triggered!");
-        injectProfileButton();
+        injectDebugText();
     };
 
     plugin.onUnload = function () {
-        console.log("[Testing Plugin] Plugin onUnload triggered!");
         if (unpatch) unpatch();
     };
 
