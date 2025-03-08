0ff4b55b26a7fbd8e825179e1727473e1b01b686
Drcatto <coiersill0@gmail.com>
2025-03-08 14:30:44 -0700

    Update index.js

--- Testing/index.js
+++ Testing/index.js
@@ -1,6 +1,8 @@
 (function (plugin, v, m) {
     "use strict";
 
+    console.log("[Testing Plugin] Plugin loaded and running!");
+
     const { React } = v.metro.common;
     const ProfileModule = m.findByProps("openProfileSheet");
     const UserStore = m.findByProps("getCurrentUser");
@@ -9,26 +11,28 @@
     let unpatch;
 
     function injectProfileButton() {
-        console.log("[ProfileButton] Injecting into ChatInputActions...");
-        
+        console.log("[Testing Plugin] Attempting to inject profile button...");
+
         if (!ChatInputActions) {
-            console.error("[ProfileButton] ChatInputActions not found!");
+            console.error("[Testing Plugin] ChatInputActions component not found!");
             return;
         }
 
         unpatch = v.patcher.after("default", ChatInputActions, (_, res) => {
+            console.log("[Testing Plugin] ChatInputActions found, modifying...");
+
             if (!res || !res.props || !res.props.children) {
-                console.error("[ProfileButton] ChatInputActions has no children!");
+                console.error("[Testing Plugin] ChatInputActions has no children!");
                 return res;
             }
 
             const currentUser = UserStore.getCurrentUser();
             if (!currentUser) {
-                console.error("[ProfileButton] Unable to get current user!");
+                console.error("[Testing Plugin] Unable to get current user!");
                 return res;
             }
 
-            console.log("[ProfileButton] Adding avatar button...");
+            console.log("[Testing Plugin] Injecting avatar button...");
 
             const avatarUrl = `https://cdn.discordapp.com/avatars/${currentUser.id}/${currentUser.avatar}.png?size=32`;
 
@@ -50,17 +54,20 @@
 
             // Inject the button at the **start** of the action buttons
             res.props.children.unshift(AvatarButton);
+            console.log("[Testing Plugin] Profile button added!");
             return res;
         });
 
-        console.log("[ProfileButton] Injection complete.");
+        console.log("[Testing Plugin] Injection complete.");
     }
 
     plugin.onLoad = function () {
+        console.log("[Testing Plugin] Plugin onLoad triggered!");
         injectProfileButton();
     };
 
     plugin.onUnload = function () {
+        console.log("[Testing Plugin] Plugin onUnload triggered!");
         if (unpatch) unpatch();
     };
 
